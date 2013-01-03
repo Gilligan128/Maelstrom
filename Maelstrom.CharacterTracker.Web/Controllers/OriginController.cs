9af1d776817a5e296d4e3e49fe498e2d3ade111c
Gilligan <gilligan128@gmail.com>
2013-01-02 23:52:11 -0500

    Added some basic scaffolding for simulating dynamic data.

---
+++ Maelstrom.CharacterTracker.Web/Controllers/OriginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Maelstrom.CharacterTracker.Web.Models;
+
+namespace Maelstrom.CharacterTracker.Web.Controllers
+{
+    public class OriginController : LookupController<Origin>
+    {
+
+        static OriginController()
+        {
+            Add(new Origin {  Name = "Orlo" }, new Origin {  Name = "Gay Telbri" });
+        }
+
+
+    }
+
+
+}
