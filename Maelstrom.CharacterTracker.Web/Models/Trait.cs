6fb40579e4e9e2b05b71cb11b3ec8fe32899e576
Gilligan <gilligan128@gmail.com>
2013-01-19 13:41:04 -0500

    yay files

---
+++ Maelstrom.CharacterTracker.Web/Models/Trait.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Maelstrom.CharacterTracker.Web.Models
+{
+	public class Trait : Entity
+	{
+ 
+        [Required]
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        [Range(0,10)]
+        public virtual int UsesPerDay { get; set; }
+        [Range(0,10)]
+        public virtual int HpBonus { get; set; }
+        [Range(0,10)]
+        public virtual int ApBonus { get; set; }
+	}
+}
